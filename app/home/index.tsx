07677e0fce8c8e94260318d500798c26de82687d
donCelis <alexcelis019@gmail.com>
2024-05-14 14:51:51 -0500

    refactor styles home

--- app/home/index.tsx
+++ app/home/index.tsx
@@ -2,8 +2,6 @@ import { StyleSheet, View, useColorScheme } from "react-native";
 
 import { useLocalSearchParams, useRouter } from "expo-router";
 import { ScrollView } from "react-native-gesture-handler";
-import { wp } from "@/utils/dimensions";
-import { sizes } from "@/constants/metrics";
 import { useGetMovies } from "@/hooks/useGetMovies";
 import { GenresTabs } from "@/components/GenresTabs";
 import { useState } from "react";
@@ -27,7 +25,6 @@ export default function HomeScreen() {
       )}
       <ScrollView showsVerticalScrollIndicator={false}>
         <View style={styles.container}>
-          {/* Movies */}
           {!isLoading && hasMovies && <MoviesGrid currentTab={currentTab} />}
         </View>
       </ScrollView>
@@ -38,12 +35,6 @@ export default function HomeScreen() {
 const styles = StyleSheet.create({
   container: {
     minHeight: 3,
-    width: wp(100),
-    paddingTop: sizes.level_8,
-    paddingVertical: sizes.level_4,
-    gap: sizes.level_3,
-  },
-  listContainerStyle: {
-    paddingHorizontal: sizes.level_4,
+    width: "100%",
   },
 });
